b1407b4f2bd22c172ced4361c77b76b41c453f1f
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2026-09-26 00:46:00 +0800

    Update twrp_whyred.mk

--- twrp_whyred.mk
+++ twrp_whyred.mk
@@ -31,13 +31,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Redmi Note 5 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-# enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.secure=1 \
-    ro.adb.secure=0 \
-    ro.allow.mock.location=0 \
-    ro.hardware.keystore=sdm660 \
-    ro.hardware.gatekeeper=sdm660
-
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/xiaomi/whyred/device.mk)
